545488519e45aae96d3c73e64995d5e635673614
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-23 09:16:48 -0500

    added color list view

--- SimpleReminders/Views/AddNewListView.swift
+++ SimpleReminders/Views/AddNewListView.swift
@@ -9,26 +9,35 @@ import SwiftUI
 
 struct AddNewListView: View {
     
+    // MARK:  Properties
     @ObservedObject private var viewModel: AddNewListViewModel
     
     init(viewModel: AddNewListViewModel) {
         self.viewModel = viewModel
     }
     
+    // MARK:  Body
     var body: some View {
         Form {
-            Text("New List")
-                .font(.headline)
-                .padding(.bottom, 20)
-            
-            HStack {
-                Text("Name")
-                TextField("", text: $viewModel.name)
-            }
-        }
+            VStack {
+             
+                Text("New List")
+                    .font(.headline)
+                    .padding(.bottom, 20)
+                
+                HStack {
+                    Text("Name")
+                    TextField("", text: $viewModel.name)
+                }
+                
+            } // End of VStack
+        } // End of Form
+        .frame(minWidth: 300)
+        .padding()
     }
 }
 
+// MARK:  Preview
 struct AddNewListView_Previews: PreviewProvider {
     static var previews: some View {
         let viewContext = CoreDataManager.shared.persistentContainer.viewContext
---
+++ SimpleReminders/Views/ColorListView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorListView.swift
+//  SimpleReminders
+//
+//  Created by Thomas Cowern New on 2/23/22.
+//
+
+import SwiftUI
+
+struct ColorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorListView()
+    }
+}
