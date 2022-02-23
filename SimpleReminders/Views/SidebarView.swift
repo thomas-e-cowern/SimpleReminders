4537016e52990ff93d4925e234f8f4c1578253bb
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-23 07:08:25 -0500

    added sidebar view

---
+++ SimpleReminders/Views/SidebarView.swift
@@ -0,0 +1,20 @@
+//
+//  SidebarView.swift
+//  SimpleReminders
+//
+//  Created by Thomas Cowern New on 2/23/22.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SidebarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SidebarView()
+    }
+}
