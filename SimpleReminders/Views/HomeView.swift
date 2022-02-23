ca62d42798352996e5dc59f72114504f0ee746e6
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-23 07:04:43 -0500

    added home view

---
+++ SimpleReminders/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SimpleReminders
+//
+//  Created by Thomas Cowern New on 2/23/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
