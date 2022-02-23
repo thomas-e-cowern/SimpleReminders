221a6b17950fbd9aadb43de21cb6a61d8cdc7358
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-23 07:01:00 -0500

    ininital commit

---
+++ SimpleReminders/SimpleRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRemindersApp.swift
+//  SimpleReminders
+//
+//  Created by Thomas Cowern New on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
