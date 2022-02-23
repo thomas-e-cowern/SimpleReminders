09a04dfcb30494413da393306e2f8d0f05e7633f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-23 08:05:16 -0500

    added manual model

---
+++ SimpleReminders/Models/RemiderList.swift
@@ -0,0 +1,30 @@
+//
+//  RemiderList.swift
+//  SimpleReminders
+//
+//  Created by Thomas Cowern New on 2/23/22.
+//
+
+import Foundation
+import CoreData
+import AppKit
+
+@objc (ReminderList)
+class ReminderList: NSManagedObject {
+    
+}
+
+extension ReminderList {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ReminderList> {
+        return NSFetchRequest<ReminderList>(entityName: "ReminderList")
+    }
+
+    @NSManaged public var color: NSColor?
+    @NSManaged public var name: String?
+
+}
+
+extension ReminderList : Identifiable {
+
+}
